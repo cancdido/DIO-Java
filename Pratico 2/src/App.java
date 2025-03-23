0634631a40e01a5e36b8f65c9ce0fa1cf9719b61
Paulo Candido <cancdido@gmail.com>
2025-03-23 12:01:44 -0300

    Update

---
+++ Pratico 2/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Area quadrilatero:");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+        
+    }
+}
