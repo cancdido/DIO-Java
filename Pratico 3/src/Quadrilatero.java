579575c9fc9ee54f3c7c1b480a1b000005c48eb0
Paulo Candido <cancdido@gmail.com>
2025-03-23 12:30:37 -0300

    Pratico 3

---
+++ Pratico 3/src/Quadrilatero.java
@@ -0,0 +1,16 @@
+//Diferente do Pratico 2, Agora os metodos irao retornar valores.
+
+public class Quadrilatero {
+
+    public static double area(double lado) {
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+        return ((baseMaior + baseMenor) * altura) / 2;
+    }
+}
